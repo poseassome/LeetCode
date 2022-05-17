582124f9fe4ec73a450109c50a4c57360a68ee39
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-05-17 22:32:40 +0900

    Time: 240 ms (19.59%), Space: 46.5 MB (92.18%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let answer = 0;
+    const value = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000 
+    }
+
+    for(let i=0; i<s.length; i++){
+        let current = value[s[i]];
+        let next = value[s[i+1]];
+        
+        if(current < next) {
+            answer += next - current;
+            i++;
+        }
+        else answer += current;
+    }
+    return answer;
+};
