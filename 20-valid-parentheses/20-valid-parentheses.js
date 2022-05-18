e50da62cd6f38ff6283e014f7650f1d95d6071cf
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-05-18 23:11:22 +0900

    Time: 80 ms (53.63%), Space: 42.3 MB (59.71%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+      let stack = [];
+      for (let i = 0; i < s.length; i += 1) {
+        const top = stack[stack.length - 1];
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+          stack.push(s[i]);
+        } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
+          stack.pop();
+        } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
+          stack.pop();
+        } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+      return stack.length === 0;
+};
