6c09ae863d38b79984ab9b5fc8daeb18f8a13fd8
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-05-30 23:33:45 +0900

    Time: 82 ms (51.16%), Space: 42.4 MB (88.01%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -8,9 +8,7 @@ var intersection = function(nums1, nums2) {
     for(let i=0; i<nums1.length; i++){
         if(nums2.includes(nums1[i]) && !arr.includes(nums1[i])) {
             arr.push(nums1[i]);
-            // nums1.splice(i,1);
             nums2.splice(nums2.indexOf(nums1[i]), 1);
-            // i--;
         }
     }
     return arr;
