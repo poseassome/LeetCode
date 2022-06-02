df789026e8758ab0086d3f8adf3166fc78d7d80c
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-06-02 23:32:15 +0900

    Time: 124 ms (5.25%), Space: 42 MB (70.17%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    s = s.split("");
+    t = t.split("");
+    console.log(s)
+    while(t.length){
+        if(t[0] === s[0]) {
+            s.shift();
+            t.shift();
+        } else t.shift();
+    }
+    return s.length === 0;
+};
