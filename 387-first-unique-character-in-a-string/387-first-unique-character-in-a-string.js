318df4f3d6941f621996da529c9ada23ff8dbc08
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-06-17 23:36:24 +0900

    Time: 153 ms (43.97%), Space: 45.2 MB (73.30%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -3,18 +3,25 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let answer = -1;
-    let sH = new Map();
-    s = s.split('');
-    for(let x of s){
-        if(sH.has(x)) sH.set(x, sH.get(x)+1);
-        else sH.set(x, 1)
-    }
-    for(let [key, val] of sH){
-        if(val === 1){
-            answer = s.indexOf(key);
-            break;
+    // let answer = -1;
+    // let sH = new Map();
+    // s = s.split('');
+    // for(let x of s){
+    //     if(sH.has(x)) sH.set(x, sH.get(x)+1);
+    //     else sH.set(x, 1)
+    // }
+    // for(let [key, val] of sH){
+    //     if(val === 1){
+    //         answer = s.indexOf(key);
+    //         break;
+    //     }
+    // }
+    // return answer;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return i;
         }
     }
-    return answer;
+    return -1;
 };
