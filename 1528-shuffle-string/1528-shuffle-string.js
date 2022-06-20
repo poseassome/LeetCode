f708c22bb21c261476110b569e65300df4b28f26
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-06-20 21:08:17 +0900

    Time: 142 ms (5.85%), Space: 44.2 MB (76.77%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let answer = '';
+    for (i=0; i<indices.length; i++) {
+        for (j=0; j<indices.length; j++) {
+            if(indices[j] === i) {
+                answer += s[j];
+            }
+        }
+    }
+    return answer;
+};
