01dec0379882e54f9ee77b4ff48f3f78c72b3a89
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-07-04 14:04:11 +0900

    Time: 99 ms (67.34%), Space: 44.2 MB (88.20%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let odd = [];
+    let even = [];
+
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] % 2 == 0) even.push(nums[i]);
+        else odd.push(nums[i]);
+    }
+    return nums = even.concat(odd);
+};
