7cc21d99aa5c5eb8f45a29f2f4502e313c05cdff
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-07-08 20:07:06 +0900

    Time: 150 ms (7.68%), Space: 43.7 MB (42.70%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if(n===1) return true;
+    
+    const factors = [2, 3, 5];
+    for(let i=0; i<factors.length; i++){
+        while(n%factors[i]===0 && n!==0){
+            n = n/factors[i];
+        }
+    }
+     return n === 1
+};
