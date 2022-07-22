2f397686945ee11150a86bcb67d3bb60e6554865
jiyoung <93672310+poseassome@users.noreply.github.com>
2022-07-22 20:43:41 +0900

    Time: 132 ms (13.69%), Space: 43.2 MB (92.81%) - LeetHub

---
+++ 169-majority-element/169-majority-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const hash = {};
+    let max = 0, val;
+
+    for (let i=0; i<nums.length; i++){
+        hash[nums[i]] ? hash[nums[i]]++ : hash[nums[i]] = 1;
+        if (hash[nums[i]] > max){
+            max = hash[nums[i]];
+            val = nums[i];
+        }
+    }
+    return val
+};
