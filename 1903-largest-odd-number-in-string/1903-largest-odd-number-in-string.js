39ee8c045f96d60972250db55f079b1ed4369433
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-01-09 19:48:54 +0900

    Time: 64 ms (97.13%), Space: 45.4 MB (77.59%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    if(Number(num.slice(-1))%2 !== 0) return num;
+    for(let i=num.length-1; i>0; i--){
+        if(parseInt(num[i-1])%2 !== 0) return num.substr(0, i);
+    }
+    return '';
+};
