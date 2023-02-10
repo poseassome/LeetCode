12a0b3b518a14ee41936fa938bf547eab193e617
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-02-10 21:34:43 +0900

    Time: 208 ms (25.62%), Space: 48.2 MB (27.76%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} gifts
+ * @param {number} k
+ * @return {number}
+ */
+var pickGifts = function(gifts, k) {
+    for(let i=0; i<k; i++){
+        gifts.sort((a,b) => b-a);
+        gifts.push(Math.floor(Math.sqrt(gifts[0])));
+        gifts = gifts.slice(1);
+    }
+    return gifts.reduce((acc, cur) => acc+cur);
+};
