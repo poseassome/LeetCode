7da50783c680c3dfa259e18d0fa2ac7bf132b768
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-03-14 00:15:16 +0900

    Time: 58 ms (82.45%), Space: 42.3 MB (21.94%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+    n = n.toString(2);
+    for(let i=0; i<n.length; i++){
+        if(n[i] === n[i+1]) return false;
+    }
+    return true;
+};
