1e4b03eb745b086d7349c401a794504279822ef4
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-05-11 00:28:31 +0900

    Time: 135 ms (29.63%), Space: 59.1 MB (6.48%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
@@ -3,17 +3,13 @@
  * @return {boolean}
  */
 var checkValid = function(matrix) {
-
-
-
-    const n = matrix.length;
-    const rows = Array(n).fill().map(_ => new Set());
-    const cols = Array(n).fill().map(_ => new Set());
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
+    const rows = Array(matrix.length).fill().map(el => new Set());
+    const cols = Array(matrix.length).fill().map(el => new Set());
+    for(let i=0; i<matrix.length; i++){
+        for(let j=0; j<matrix.length; j++){
             rows[i].add(matrix[i][j]);
             cols[j].add(matrix[i][j]);
         }
     }
-    return rows.every(s => s.size === n) && cols.every(s => s.size === n);
+    return rows.every(el => el.size === matrix.length) && cols.every(el => el.size === matrix.length)
 };
