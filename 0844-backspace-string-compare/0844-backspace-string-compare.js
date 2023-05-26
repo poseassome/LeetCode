0135b7a6ee771ca091614641c0ab15266f04d0c2
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-05-26 16:55:36 +0900

    Time: 63 ms (52.54%), Space: 44.2 MB (12.33%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -10,10 +10,7 @@ var backspaceCompare = function(s, t) {
             if(x === '#' && stack[stack.length-1] !== '#' && stack.length) stack.pop();
             else if(x !== '#') stack.push(x);
         }
-        if(stack[0] === '#') stack.shift();
         return stack.join('');
     }
-    console.log(backspace(s))
-    console.log(backspace(t))
     return backspace(s) === backspace(t);
 };
