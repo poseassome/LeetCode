5dad59d17fb49370fc2561fa95e7fd3663007afb
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-06-11 00:09:03 +0900

    Time: 57 ms (78.87%), Space: 43.3 MB (18.56%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function(name, typed) {
+    let idx = 0;
+    for(let i=0; i<typed.length; i++){
+        if(typed[i] === name[idx]) idx++;
+        else if(typed[i] === name[idx-1]) continue;
+        else return false;
+    }
+    return idx === name.length;
+};
