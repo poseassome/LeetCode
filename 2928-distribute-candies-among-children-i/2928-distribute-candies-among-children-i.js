2b7ba6c31fe50cf8e72599e05f4bafe27f76f230
jiyoung <93672310+poseassome@users.noreply.github.com>
2023-11-16 11:41:26 +0900

    Time: 81 ms (22.76%), Space: 42.4 MB (73.79%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @param {number} limit
+ * @return {number}
+ */
+var distributeCandies = function(n, limit) {
+    let res = 0;
+    for(let i=0; i<=limit; i++){
+        for(let j=0; j<=limit; j++){
+            for(let k=0; k<=limit; k++){
+                if(i+j+k === n) res++;
+            }
+        }
+    }
+    return res;
+};
