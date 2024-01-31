78dee9f3be374189524d3b39f7949d0f7a1e7dca
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-01-31 23:03:25 +0900

    Time: 2747 ms (5.14%), Space: 64.8 MB (12.62%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+    nums.sort((a,b) => a-b);
+    let res = -Infinity;
+    while(nums.length){
+        res = Math.max(nums.pop()+nums.shift(), res);
+    }
+    return res;
+};
