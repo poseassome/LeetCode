1e786775e8cc18328a909e483468f8a88bf5d346
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-04-12 00:15:55 +0900

    Time: 70 ms (43.28%), Space: 56.8 MB (81.96%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0, right = height.length-1, max = 0;
+    while(left < right){
+        max = Math.max(max, (right-left)*Math.min(height[left], height[right]));
+        if(height[left] < height[right]) left++;
+        else right--;
+    }
+    return max;
+};
