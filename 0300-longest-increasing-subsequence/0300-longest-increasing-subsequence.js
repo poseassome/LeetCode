a501bf26899a7b74129870937213115bf399dcf1
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-05-22 20:22:27 +0900

    Time: 126 ms (71.44%), Space: 50.5 MB (65.06%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let arr = new Array(nums.length).fill(1);
+    for(let i=1; i<nums.length; i++){
+        for(let j=0; j<i; j++){
+            if(nums[j]<nums[i]) arr[i] = Math.max(arr[i], 1+arr[j]);
+        }
+    }
+    return Math.max(...arr);
+};
