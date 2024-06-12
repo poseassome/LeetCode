790ada7a4da0c778ea7675c51bb38ab11eb5f5fb
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-06-13 07:47:11 +0900

    Time: 70 ms (53.39%), Space: 52 MB (42.63%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var resultArray = function(nums) {
+    let arr1 = [nums[0]], arr2 = [nums[1]];
+    for(let i=2; i<nums.length; i++){
+        if(arr1.at(-1) > arr2.at(-1)) arr1.push([nums[i]]);
+        else arr2.push(nums[i]);
+    }
+    return [...arr1, ...arr2];
+};
