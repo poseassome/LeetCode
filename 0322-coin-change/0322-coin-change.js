f78121afdd69868b9a24b98adb32cf23caf9828d
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-06-26 21:56:14 +0900

    Time: 95 ms (65.78%), Space: 53.6 MB (88.50%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const dp = new Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    for(let i=0; i<coins.length; i++){
+        for(let j=coins[i]; j<=amount; j++){
+            dp[j] = Math.min(dp[j], 1+dp[j-coins[i]]);
+        }
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount];
+};
