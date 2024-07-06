c022daed89903cab56d38fbac42ed005b3febeef
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-07-06 12:24:44 +0900

    Time: 90 ms (64.60%), Space: 58.8 MB (84.37%) - LeetHub

---
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+    let cnt = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] === 0){
+            if(i>nums.length-3) return -1;
+            nums[i] = 1;
+            nums[i+1] = nums[i+1] ? 0 : 1;
+            nums[i+2] = nums[i+2] ? 0 : 1;
+            cnt++;
+        }
+    }
+    return cnt;
+};
