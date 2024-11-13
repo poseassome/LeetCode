a635f3452f49970cdef16313249359fb5207a1d3
jiyoung <93672310+poseassome@users.noreply.github.com>
2024-11-13 21:47:59 +0900

    Time: 0 ms (100.00%), Space: 55.7 MB (75.18%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    let left = 0, right = arr.length-k;
+    while(left<right){
+        const mid = left + Math.floor((right-left)/2);
+        if(x-arr[mid] > arr[mid+k]-x) left = mid+1;
+        else right = mid;
+    }
+    return arr.slice(left, left+k);
+};
