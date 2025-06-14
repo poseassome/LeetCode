bd02f2d77b00ac2cabd19be256c1e8ecde8b25bb
jiyoung <93672310+poseassome@users.noreply.github.com>
2025-06-15 08:53:43 +0900

    Time: 1 ms (93.75%), Space: 55.9 MB (47.92%) - LeetHub

---
+++ 1743-count-substrings-that-differ-by-one-character/1743-count-substrings-that-differ-by-one-character.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var countSubstrings = function(s, t) {
+    let res = 0;
+    for(let i=0; i<s.length; i++){
+        for(let j=0; j<t.length; j++){
+            let diff = 0;
+            let x = i, y = j;
+            while(x < s.length && y < t.length){
+                if(s[x] !== t[y]) diff++;
+                if(diff === 1) res++;
+                if(diff > 1) break;
+                x++;
+                y++;
+            }
+        }
+    }
+    return res;
+};
