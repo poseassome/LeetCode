c6b639cd82b5a28d3c50608122910dc45e454ffa
jiyoung <93672310+poseassome@users.noreply.github.com>
2025-07-13 10:59:45 +0900

    Time: 2 ms (100%), Space: 55.5 MB (40%) - LeetHub

---
+++ 1971-incremental-memory-leak/1971-incremental-memory-leak.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} memory1
+ * @param {number} memory2
+ * @return {number[]}
+ */
+var memLeak = function(memory1, memory2) {
+    let i = 1;
+    while(1){
+        if(memory1 >= memory2){
+            if(memory1 < i) break;
+            memory1 -= i;
+        } else {
+            if(memory2 < i) break;
+            memory2 -= i;
+        }
+        i++;
+    }
+    return [i, memory1, memory2];
+};
