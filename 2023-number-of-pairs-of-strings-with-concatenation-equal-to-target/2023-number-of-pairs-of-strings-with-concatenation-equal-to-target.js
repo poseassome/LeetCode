c1d4ae80c9e00a180fe6665b9529e0b7f2731114
jiyoung <93672310+poseassome@users.noreply.github.com>
2026-02-12 11:19:29 +0900

    Time: 6 ms (77.36%), Space: 59.3 MB (73.58%) - LeetHub

---
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} nums
+ * @param {string} target
+ * @return {number}
+ */
+var numOfPairs = function(nums, target) {
+    let res = 0;
+    for(let i=0; i<nums.length; i++){
+        for(let j=0; j<nums.length; j++){
+            if(i !== j && nums[i] + nums[j] === target) res++;
+        }
+    }
+    return res;
+};
